f4d27792a9baf36f43d4652815e45e399d61b19c
kpugame <warimo47@naver.com>
2018-05-29 18:40:56 +0900

    Fix DetailTableView

--- UniversityInfomation/UniversityInfomation/DetailTableViewController.swift
+++ UniversityInfomation/UniversityInfomation/DetailTableViewController.swift
@@ -8,115 +8,12 @@
 
 import UIKit
 
-class DetailTableViewController: UITableViewController, XMLParserDelegate
+class DetailTableViewController: UITableViewController
 {
     @IBOutlet var DetailTableView: UITableView!
     
-    var parser = XMLParser()
-    
-    var posts = NSMutableArray()
-    var subPosts = NSMutableArray()
-    
-    var elements = NSMutableDictionary()
-    var element = NSString()
-    
-    var UniversityName = NSMutableString()
-    var DepartmentName = NSMutableString()
-    var DanDivName = NSMutableString()
-    var MJRStateName = NSMutableString()
-    
-    let strUniInfoUrl = "https://openapi.gg.go.kr/Univmjrm?key=6ec3d4fbb5234452be28d5868db20223&psize=1000&pindex=1"
-    
-    func beginParsing()
-    {
-        posts = []
-        subPosts = []
-        
-        /*
-        for i in 1..<10
-        {
-            let url = strUniInfoUrl + String(i)
-            
-            // print(url)
-            parser = XMLParser(contentsOf: URL(string: url)!)!
-            parser.delegate = self
-            parser.parse()
-        }
-         */
-        
-        parser = XMLParser(contentsOf: URL(string: strUniInfoUrl)!)!
-        parser.delegate = self
-        parser.parse()
-        
-        // DetailTableView!.reloadData()
-    }
-    
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String])
-    {
-        element = elementName as NSString
-        
-        if (elementName as NSString).isEqual(to: "row")
-        {
-            elements = NSMutableDictionary()
-            elements = [ : ]
-            UniversityName = NSMutableString()
-            UniversityName = ""
-            DepartmentName = NSMutableString()
-            DepartmentName = ""
-            DanDivName = NSMutableString()
-            DanDivName = ""
-            MJRStateName = NSMutableString()
-            MJRStateName = ""
-        }
-    }
-    
-    func parser(_ parser: XMLParser, foundCharacters string: String)
-    {
-        if element.isEqual(to: "SCHOOL_NM")
-        {
-            UniversityName.append(string)
-        }
-        else if element.isEqual(to: "UNDSTUD_KWA_NM")
-        {
-            DepartmentName.append(string)
-        }
-        else if element.isEqual(to: "DAN_DIV_NM")
-        {
-            DanDivName.append(string)
-        }
-        else if element.isEqual(to: "MJR_STATE_NM")
-        {
-            MJRStateName.append(string)
-        }
-    }
-    
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
-    {
-        if (elementName as NSString).isEqual(to: "row")
-        {
-            if !UniversityName.isEqual(nil)
-            {
-                elements.setObject(UniversityName, forKey: "SCHOOL_NM" as NSCopying)
-            }
-            
-            if !DepartmentName.isEqual(nil)
-            {
-                elements.setObject(DepartmentName, forKey: "UNDSTUD_KWA_NM" as NSCopying)
-            }
-            
-            if !DanDivName.isEqual(nil)
-            {
-                elements.setObject(DanDivName, forKey: "DAN_DIV_NM" as NSCopying)
-            }
-            
-            if !MJRStateName.isEqual(nil)
-            {
-                elements.setObject(MJRStateName, forKey: "MJR_STATE_NM" as NSCopying)
-            }
-            
-            posts.add(elements)
-        }
-    }
+    let postsname: [String] = ["학교명", "소재 지역", "학교 구분", "공사립 구분", "소재지 지번 주소", "소재지 도로명 주소", "우편 주소"]
+    var posts: [String] = ["", "", "", "", "", "", ""]
     
     override func viewDidLoad()
     {
@@ -127,8 +24,6 @@ class DetailTableViewController: UITableViewController, XMLParserDelegate
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-        beginParsing()
     }
 
     override func didReceiveMemoryWarning()
@@ -148,15 +43,17 @@ class DetailTableViewController: UITableViewController, XMLParserDelegate
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of rows
-        return posts.count
+        return 7
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DepartmentName", for: indexPath)
         
-        cell.textLabel?.text = posts[indexPath.row] as? String
-        cell.detailTextLabel?.text = subPosts[indexPath.row] as? String
+        cell.textLabel?.text = postsname[indexPath.row]
+        cell.textLabel?.textColor = UIColor.black
+        cell.detailTextLabel?.text = posts[indexPath.row]
+        cell.detailTextLabel?.textColor = UIColor.black
         
         return cell
     }
--- UniversityInfomation/UniversityInfomation/TableViewController.swift
+++ UniversityInfomation/UniversityInfomation/TableViewController.swift
@@ -18,6 +18,12 @@ class TableViewController: UITableViewController, XMLParserDelegate
     var element = NSString()
     
     var UniversityName = NSMutableString()
+    var SIGUN_NM = NSMutableString()
+    var SCHOOL_DIV_NM = NSMutableString()
+    var PLVTINST_DIV_NM = NSMutableString()
+    var REFINE_LOTNO_ADDR = NSMutableString()
+    var REFINE_ROADNM_ADDR = NSMutableString()
+    var REFINE_ZIP_CD = NSMutableString()
     var Addr = NSMutableString()
     var XPos = NSMutableString()
     var YPos = NSMutableString()
@@ -49,6 +55,18 @@ class TableViewController: UITableViewController, XMLParserDelegate
             elements = [ : ]
             UniversityName = NSMutableString()
             UniversityName = ""
+            SIGUN_NM = NSMutableString()
+            SIGUN_NM = ""
+            SCHOOL_DIV_NM = NSMutableString()
+            SCHOOL_DIV_NM = ""
+            PLVTINST_DIV_NM = NSMutableString()
+            PLVTINST_DIV_NM = ""
+            REFINE_LOTNO_ADDR = NSMutableString()
+            REFINE_LOTNO_ADDR = ""
+            REFINE_ROADNM_ADDR = NSMutableString()
+            REFINE_ROADNM_ADDR = ""
+            REFINE_ZIP_CD = NSMutableString()
+            REFINE_ZIP_CD = ""
             Addr = NSMutableString()
             Addr = ""
             XPos = NSMutableString()
@@ -64,9 +82,30 @@ class TableViewController: UITableViewController, XMLParserDelegate
         {
             UniversityName.append(string)
         }
+        else if element.isEqual(to: "SIGUN_NM")
+        {
+            SIGUN_NM.append(string)
+        }
+        else if element.isEqual(to: "SCHOOL_DIV_NM")
+        {
+            SCHOOL_DIV_NM.append(string)
+        }
+        else if element.isEqual(to: "PLVTINST_DIV_NM")
+        {
+            PLVTINST_DIV_NM.append(string)
+        }
+        else if element.isEqual(to: "REFINE_LOTNO_ADDR")
+        {
+            REFINE_LOTNO_ADDR.append(string)
+        }
         else if element.isEqual(to: "REFINE_ROADNM_ADDR")
         {
             Addr.append(string)
+            REFINE_ROADNM_ADDR.append(string)
+        }
+        else if element.isEqual(to: "REFINE_ZIP_CD")
+        {
+            REFINE_ZIP_CD.append(string)
         }
         else if element.isEqual(to: "REFINE_WGS84_LAT")
         {
@@ -85,6 +124,36 @@ class TableViewController: UITableViewController, XMLParserDelegate
             if !UniversityName.isEqual(nil)
             {
                 elements.setObject(UniversityName, forKey: "FACLT_NM" as NSCopying)
+            }
+            
+            if !SIGUN_NM.isEqual(nil)
+            {
+                elements.setObject(SIGUN_NM, forKey: "SIGUN_NM" as NSCopying)
+            }
+            
+            if !SCHOOL_DIV_NM.isEqual(nil)
+            {
+                elements.setObject(SCHOOL_DIV_NM, forKey: "SCHOOL_DIV_NM" as NSCopying)
+            }
+            
+            if !PLVTINST_DIV_NM.isEqual(nil)
+            {
+                elements.setObject(PLVTINST_DIV_NM, forKey: "PLVTINST_DIV_NM" as NSCopying)
+            }
+            
+            if !REFINE_LOTNO_ADDR.isEqual(nil)
+            {
+                elements.setObject(REFINE_LOTNO_ADDR, forKey: "REFINE_LOTNO_ADDR" as NSCopying)
+            }
+            
+            if !REFINE_ROADNM_ADDR.isEqual(nil)
+            {
+                elements.setObject(REFINE_ROADNM_ADDR, forKey: "REFINE_ROADNM_ADDR" as NSCopying)
+            }
+            
+            if !REFINE_ZIP_CD.isEqual(nil)
+            {
+                elements.setObject(REFINE_ZIP_CD, forKey: "REFINE_ZIP_CD" as NSCopying)
             }
             
             if !Addr.isEqual(nil)
@@ -160,6 +229,20 @@ class TableViewController: UITableViewController, XMLParserDelegate
                 mapViewController.MapY = MapY
             }
         }
+        
+        if segue.identifier == "segueToDetailView"
+        {
+            if let detailTableViewController = segue.destination as? DetailTableViewController
+            {
+                detailTableViewController.posts =
+                    [UniversityName as String,
+                     SIGUN_NM as String,
+                     SCHOOL_DIV_NM as String,
+                     PLVTINST_DIV_NM as String,
+                     REFINE_LOTNO_ADDR as String,
+                     REFINE_ROADNM_ADDR as String,
+                     REFINE_ZIP_CD as String]
+            }
+        }
     }
-
 }
