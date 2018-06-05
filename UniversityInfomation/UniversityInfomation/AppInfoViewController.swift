71ef4cdcc922ae906c369f5360ca8932694ab118
kpugame <warimo47@naver.com>
2018-06-05 18:46:06 +0900

    Add tap gesture in InfomationView

---
+++ UniversityInfomation/UniversityInfomation/AppInfoViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AppInfoViewController.swift
+//  UniversityInfomation
+//
+//  Created by kpugame on 2018. 6. 5..
+//  Copyright © 2018년 BKS. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoViewController: UIViewController
+{
+    @IBOutlet var developerInfoView: UITextView!
+    @IBOutlet var appInfoView: UITextView!
+    
+    var isAppInfoViewShowing = true
+    
+    @IBAction func InfoViewToggle(gesture: UITapGestureRecognizer?)
+    {
+        if isAppInfoViewShowing
+        {
+            UIView.transition(from: appInfoView,
+                              to: developerInfoView,
+                              duration: 1.0,
+                              options: [UIViewAnimationOptions.transitionFlipFromLeft, UIViewAnimationOptions.showHideTransitionViews],
+                              completion: nil)
+            self.title = "개발자 정보"
+        }
+        else
+        {
+            UIView.transition(from: developerInfoView,
+                             to: appInfoView,
+                             duration: 1.0,
+                             options: [UIViewAnimationOptions.transitionFlipFromRight, UIViewAnimationOptions.showHideTransitionViews],
+                             completion: nil)
+            self.title = "어플 정보"
+        }
+        
+        isAppInfoViewShowing = !isAppInfoViewShowing
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
